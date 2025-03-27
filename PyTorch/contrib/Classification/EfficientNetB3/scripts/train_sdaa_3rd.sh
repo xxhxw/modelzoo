e7063b48d5f2f5ccc171d23e6106aec3a0515f11
Wangyu0529 <wywork@bupt.edu.cn>
2025-03-27 03:26:47 +0000

    !305 【生态活动】元碁智汇·定义未来-北京邮电大学-模型训练-在PyTorch框架上支持EfficientNet-B3在imagenet上的训练 * update PyTorch/contrib/Classification/EfficientNetB3/scripts/plot_curve.py. * update PyTorch/contrib/Classification/EfficientNetB3/examples/imagenet/mmain.py. * 删除文件 PyTorch/contrib/Classification/EfficientNetB3/examples/imagenet/m… * 删除文件 PyTorch/contrib/Classification/EfficientNetB3/examples/imagenet/c… * 删除文件 PyTorch/contrib/Classification/EfficientNetB3/examples/imagenet/t… * update PyTorch/contrib/Classification/EfficientNetB3/README.md. * 删除文件 TecoInference/contrib/detection/yolov9m * update PyTorch/contrib/Classification/EfficientNetB3/scripts/train_sdaa_3rd.sh. * update PyTorch/contrib/Classification/EfficientNetB3/requirements.txt. * update PyTorch/contrib/Classification/EfficientNetB3/README.md. * 重命名 PyTorch/contrib/Classification/EfficientNet-PyTorch 为 PyTorch/cont… * efficientnet-b3-change-plot * efficientnet-b3 * efficientnet-b3 * efficientnet-b0 * efficient-b1 * B0 * change for scripts * change for readme * Change for pr * create_README * move_folder_to_ contrib * efficientnet

---
+++ PyTorch/contrib/Classification/EfficientNetB3/scripts/train_sdaa_3rd.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# 如果你希望在遇到命令报错时“不中断脚本”，
+# 建议去掉 set -e 或在命令后面加 "|| true"。
+# 这里示例不使用 set -e，以便即使 timeout 退出也能继续下一个模型。
+# 每条命令限制6小时运行时间
+cd ..
+pip install -r requirements.txt
+cd examples/imagenet
+torchrun --nproc_per_node=4 mmain.py --arch efficientnet-b3 -b 32
