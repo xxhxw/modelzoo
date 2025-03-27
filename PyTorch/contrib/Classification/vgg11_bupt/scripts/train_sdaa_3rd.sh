d1c8668bd3303a924e21b6e6219bd6add1ad3fc1
zhenming <zhenmingzhang@bupt.edu.cn>
2025-03-27 08:06:56 +0000

    !280 【生态活动】元碁智汇·定义未来-北京邮电大学-模型训练-在PyTorch框架上支持vgg11在imagenet上的训练 * feat(Pytorch/vgg11): add Pytorch/vgg11 support * delete * feat(Pytorch/vgg11): add Pytorch/vgg11 support * update PyTorch/contrib/Classification/vgg11/README.md. * feat(PyTorch/vgg11): add PyTorch/vgg11 support * feat(PyTorch/vgg11): add PyTorch/vgg11 support * feat(PyTorch/vgg11): add PyTorch/vgg11 support * feat(PyTorch/vgg11): add PyTorch/vgg11 support * feat(PyTorch/resnet152): add PyTorch/resnet152 support * feat(PyTorch/resnet34): add PyTorch/resnet34 support * feat(PyTorch/vgg11_13_19): add PyTorch/vgg11_13_19 support * feat(PyTorch/vgg11_13_19): add PyTorch/vgg11_13_19 support * feat(PyTorch/vgg_11_13_19): add PyTorch/vgg_11_13_19 support * feat(PyTorch/alexnet): add PyTorch/alexnet support * alexnet * 新建 alexnet * 新建 bupt * 删除文件 PyTorch/contrib/bupt * 新建 alexnet * alexnet * 新建 bupt * 删除文件 PyTorch/contrib/bupt/.keep * 新建 bupt * 删除文件 PyTorch/contrib/bupt * add PyTorch/contrib.

---
+++ PyTorch/contrib/Classification/vgg11_bupt/scripts/train_sdaa_3rd.sh
@@ -0,0 +1,6 @@
+cd ..
+pip install -r requirements.txt
+
+# torchrun --nproc_per_node=1 --master_addr="127.0.0.1" --master_port=29500 main.py /data/datasets/imagenet -a vgg11 -b 256
+# torchrun --nproc_per_node=4 --master_addr="127.0.0.1" --master_port=29501 main.py /data/datasets/imagenet -a vgg11 -b 256 >> ./scripts/train_sdaa_3rd_vgg11.log 2>&1
+torchrun --nproc_per_node=4 --master_addr="127.0.0.1" --master_port=29501 main.py /data/datasets/imagenet -a vgg11 -b 256
