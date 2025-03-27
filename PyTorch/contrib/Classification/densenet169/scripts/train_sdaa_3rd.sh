f75f8466d900c6266e6c3dcd1e7b3fcba080a07a
zhenming <zhenmingzhang@bupt.edu.cn>
2025-03-27 03:11:38 +0000

    !287 【生态活动】元碁智汇·定义未来-北京邮电大学-模型训练-在PyTorch框架上支持densenet169在imagenet上的训练 * 删除文件 PyTorch/contrib/bupt * update PyTorch/contrib/Classification/densenet169/README.md. * update PyTorch/contrib/Classification/densenet169/scripts/train_sdaa_3rd.sh. * feat(PyTorch/densenet169): add PyTorch/densenet169 support * feat(PyTorch/densenet161): add PyTorch/densenet161 support * feat(PyTorch/densenet161): add PyTorch/densenet161 support * feat(PyTorch/densenet161): add PyTorch/densenet161 support * feat(PyTorch/densenet121): add PyTorch/densenet121 support * alexnet * 新建 alexnet * 新建 bupt * 删除文件 PyTorch/contrib/bupt * 新建 alexnet * alexnet * 新建 bupt * 删除文件 PyTorch/contrib/bupt/.keep * 新建 bupt * 删除文件 PyTorch/contrib/bupt * add PyTorch/contrib.

---
+++ PyTorch/contrib/Classification/densenet169/scripts/train_sdaa_3rd.sh
@@ -0,0 +1,5 @@
+cd ..
+pip install -r requirements.txt
+# torchrun --nproc_per_node=1 --master_addr="127.0.0.1" --master_port=29500 main.py /data/datasets/imagenet -a densenet169 -b 64
+# torchrun --nproc_per_node=4 --master_addr="127.0.0.1" --master_port=29502 main.py /data/datasets/imagenet -a densenet169 -b 64 >> ./scripts/train_sdaa_3rd_densenet169.log 2>&1
+torchrun --nproc_per_node=4 --master_addr="127.0.0.1" --master_port=29504 main.py /data/datasets/imagenet -a densenet169 -b 64
