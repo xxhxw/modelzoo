86d8bf1dcd9e75c32bd7333454f549f29412153b
zhaohr <zhr00596@tecorigin.com>
2025-06-13 19:01:18 +0800

    rm fixres

--- PyTorch/contrib/Classification/FixRes/imnet_evaluate/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-#
-# Adapted to tecorigin hardware
-from .train import Trainer
-from .config import TrainerConfig, ClusterConfig
